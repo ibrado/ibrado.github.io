6c340072737b0943be31b342b6226ef1b04b7baf
Alex Ibrado <alex@ibrado.org>
2017-12-10 20:41:02 +0800

    Test for hook.io

---
+++ hooks/test/index.js
@@ -0,0 +1,7 @@
+module['exports'] = function helloWorld (hook) {
+  // hook.req is a Node.js http.IncomingMessage
+  var host = hook.req.host;
+  // hook.res is a Node.js httpServer.ServerResponse
+  // Respond to the request with a simple string
+  hook.res.end('hello world');
+};
